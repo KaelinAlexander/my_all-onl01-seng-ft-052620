13dbcb8d272134c3114046dbe7236188b464f3f2
Kaelin Alexander <kaelin.alexander@gmail.com>
2020-05-30 01:01:32 +0000

    Automatically backed up by Learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -2,9 +2,12 @@ require 'pry'
 
 def my_all?(collection)
   i = 0
+  block_return_values=[]
   while i < collection.length
     i += 1
     yield(collection[i])
     i = i + 1
   end
 end
+
+my_all? ([1,2,3]) {|i| i < 2}
