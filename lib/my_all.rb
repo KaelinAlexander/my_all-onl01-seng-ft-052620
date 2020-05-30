f246778df5781f9f81faed50d8aea827852d1a28
Kaelin Alexander <kaelin.alexander@gmail.com>
2020-05-30 01:04:51 +0000

    Automatically backed up by Learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -7,13 +7,13 @@ def my_all?(collection)
     i += 1
     block_return_values << yield(collection[i])
     i = i + 1
-    
+  end
+  
     if block_return_values.include?(false)
       false
     else
       TRUE
     end
-  end
 end
 
 my_all? ([1,2,3]) {|i| i < 2}
